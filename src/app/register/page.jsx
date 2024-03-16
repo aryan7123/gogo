d535e600b1d7914d69fcc999ae5363cd63dc58db
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-03-16 23:22:39 +0530

    register page completed

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -3,8 +3,43 @@ import React from 'react'
 const page = () => {
   return (
     <>
-        <main className='login-cover flex items-center justify-center'>
-            
+        <main className='login-cover flex items-center justify-center md:px-0 px-8'>
+            <section className='bg-white overflow-hidden login-card w-[inherit] md:max-w-5xl rounded-md'>
+                <div className='flex md:flex-row flex-col items-start justify-start'>
+                    <div className='card-img w-full md:w-[40%] md:h-[86vh]'>
+                        <div className='flex flex-col justify-start items-start px-10 py-8'>
+                            <h3 className='md:text-3xl text-xl text-white font-semibold mb-3'>MAGIC IS IN THE <br /> DETAILS</h3>
+                            <span className='text-white text-sm font-medium'>Please use this form to register.</span>
+                            <span className='text-white text-sm font-medium'>If you are a member, please login.</span>
+                        </div>
+                    </div>
+                    <div className='w-full md:w-[60%]'>
+                        <div className='px-10 py-8'>
+                            <div className='gogo-logo mb-8'></div>
+                            <h3 className='text-lg font-semibold text-[#3a3a3a] uppercase'>register</h3>
+                            <div className='flex flex-col gap-2 w-full mt-8'>
+                                <label htmlFor="fullName" className='text-[#3a3a3a] text-sm font-medium'>Full Name</label>
+                                <input type="text" name='fullName' className='border p-1.5 rounded border-[#d7d7d7] text-sm text-[#3a3a3a] outline-none'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full mt-4'>
+                                <label htmlFor="email" className='text-[#3a3a3a] text-sm font-medium'>E-mail</label>
+                                <input type="email" name='email' className='border p-1.5 rounded border-[#d7d7d7] text-sm text-[#3a3a3a] outline-none'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full mt-4'>
+                                <label htmlFor="password" className='text-[#3a3a3a] text-sm font-medium'>Password</label>
+                                <input type="password" name='password' className='border p-1.5 rounded border-[#d7d7d7] text-sm text-[#3a3a3a] outline-none'/>
+                            </div>
+                            <div className='flex flex-col gap-2 w-full mt-4'>
+                                <label htmlFor="confirmPassword" className='text-[#3a3a3a] text-sm font-medium'>Confirm Password</label>
+                                <input type="password" name='confirmPassword' className='border p-1.5 rounded border-[#d7d7d7] text-sm text-[#3a3a3a] outline-none'/>
+                            </div>
+                            <div className="w-full flex items-end justify-end mt-6">
+                                <button type="button" className='uppercase bg-[#00365a] text-sm font-semibold w-28 login-btn py-2.5 text-white rounded-3xl border border-[#00365a] transition-colors hover:bg-[#001e31] hover:border-[#001e31]'>register</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     </>
   )
