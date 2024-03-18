a02214762906b722ab66a7e1b7679a54f57b0b69
Raza Ali Rahman <aryanrahman90277@gmail.com>
2024-03-18 17:18:45 +0530

    build a function for displaying the password

--- src/app/context/context.jsx
+++ src/app/context/context.jsx
@@ -5,8 +5,24 @@ import React, { useState, useEffect, useContext } from "react";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+
+    const handleShowPassword = () => {
+        setShowPassword(true);
+    }
+
+    const handleShowConfirmPassword = () => {
+        setShowConfirmPassword(true);
+    }
+
     return(
-        <AppContext.Provider value={{}}>
+        <AppContext.Provider value={{
+            showPassword,
+            showConfirmPassword,
+            handleShowPassword,
+            handleShowConfirmPassword
+        }}>
             {children}
         </AppContext.Provider>
     );
